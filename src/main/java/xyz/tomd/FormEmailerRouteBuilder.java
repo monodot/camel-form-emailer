e4d379289a81b465a63be17876f23605e7a58517
Tom Donohue <monodot@gmail.com>
2021-04-27 14:15:19 +0100

    Add predicate and configurable SMTP endpoint

--- src/main/java/xyz/tomd/FormEmailerRouteBuilder.java
+++ src/main/java/xyz/tomd/FormEmailerRouteBuilder.java
@@ -3,11 +3,17 @@ package xyz.tomd;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
 public class FormEmailerRouteBuilder extends EndpointRouteBuilder {
+
+    @Inject
+    SubmissionValidPredicate submissionIsValid;
 
     @Override
     public void configure() throws Exception {
-        //Redirect to an 'invalid' page if the user hasn't passed the antispam challenge
 
         from("platform-http:/?httpMethodRestrict=GET,POST")
                 .choice()
@@ -15,46 +21,45 @@ public class FormEmailerRouteBuilder extends EndpointRouteBuilder {
                     .log("Received POST submission")
 
                     .choice()
-                        .when(simple("${header.antispam} == {{antispam.answer}}"))
-                            .log("Passed anti-spam challenge")
+                        .when(submissionIsValid)
+                            .log("Passed validation and antispam challenge")
                             .to("direct:process-valid-response")
 
                         .otherwise()
-                            .log("Submission failed anti-spam challenge")
+                            //Redirect to an 'invalid' page if the user hasn't passed the antispam challenge
+                            .log("Submission failed validation or antispam challenge")
                             .removeHeaders("*")
                             .setHeader("Location", simple("{{redirect.fail}}"))
-                            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(303)) // Redirect 303 See Other
+                            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(303)) // Redirect 303 'See Other'
                             .transform(constant(""))
                         .endChoice()
 
                 .otherwise()
                     .transform(constant("NOOP"));
 
+
         from("direct:process-valid-response")
                 .setHeader("timestamp", simple("${date:now:yyyy-MM-dd'T'HH:mm:ss.SSSXXX}"))
 
                 // insert into SQLite here in case the email doesn't send
                 .to("sql:insert into responses (sender_name, sender_email, message, received) values (:#name, :#email, :#message, :#timestamp)")
-
                 .log("Saved into DB")
 
+                // Prepare the email content
                 .to("velocity:email.vm")
 
-                .removeHeaders("*", "email", "timestamp")
-
                 // Send mail
+                .removeHeaders("*", "email", "timestamp")
                 .setHeader("To", simple("{{mail.to}}"))
                 .setHeader("From", simple("{{mail.from}}"))
                 .setHeader("Reply-To", simple("${header.email}"))
                 .setHeader("Subject", simple("{{mail.subject}}"))
-                .to(smtps("{{smtps.host}}")
-                        .username("{{smtps.username}}")
-                        .password("{{smtps.password}}"))
+                .to("{{smtp.uri}}")
 
                 .log("Sent email to {{mail.to}}")
 
-                .removeHeader("*")
-
+                // Prepare the response
+                .removeHeaders("*")
                 .setHeader("Location", simple("{{redirect.success}}"))
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(303)) // Redirect 303 See Other after form submission
                 .transform(constant(""));
---
+++ src/main/java/xyz/tomd/SubmissionValidPredicate.java
@@ -0,0 +1,44 @@
+package xyz.tomd;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Predicate;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Predicate class to check whether a submission includes all of the mandatory fields
+ */
+@ApplicationScoped
+public class SubmissionValidPredicate implements Predicate {
+
+    public final static String ANTISPAM_FIELD = "antispam";
+
+    @ConfigProperty(name = "fields.required")
+    String fieldsRequired;
+
+    @ConfigProperty(name = "antispam.answer")
+    String antispamAnswer;
+
+    @Override
+    public boolean matches(Exchange exchange) {
+        boolean isValid = true;
+
+        // Loop through all required fields. If any are missing, then it's invalid.
+        List<String> required = Arrays.asList(fieldsRequired.split(","));
+        for (String field : required) {
+            if (exchange.getMessage().getHeader(field, "").equals("")) {
+                isValid = false;
+            }
+        }
+
+        // Also check that the antispam field is correct
+        if (!exchange.getMessage().getHeader(ANTISPAM_FIELD, "").equals(antispamAnswer)) {
+            isValid = false;
+        }
+
+        return isValid;
+    }
+}
